4a74a121cb5f2912cf1c606efd390d74e49af0b7
prajwal zingare <prajwalzingre@gmail.com>
2023-05-15 08:52:06 +0530

    feat:material Icons added

---
+++ src/assets/icons/materialIcons.js
@@ -0,0 +1,20 @@
+import PermIdentityOutlinedIcon from "@mui/icons-material/PermIdentityOutlined";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+
+import GitHubIcon from "@mui/icons-material/GitHub";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import InstagramIcon from "@mui/icons-material/Instagram";
+
+export {
+  PermIdentityOutlinedIcon,
+  FavoriteBorderIcon,
+  ShoppingCartOutlinedIcon,
+  SearchOutlinedIcon,
+  GitHubIcon,
+  TwitterIcon,
+  LinkedInIcon,
+  InstagramIcon,
+};
