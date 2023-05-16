af7680369a4d5d080f7f78e128ce63f3b75b2c57
prajwal zingare <prajwalzingre@gmail.com>
2023-05-16 21:28:39 +0530

    feat: product listing component

---
+++ src/components/productlistingpage/ProductListingPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import ProductSection from "./ProductSection/ProductSection";
+
+function ProductListingPage() {
+  return (
+    <div className="middle-content">
+      <ProductSection />
+    </div>
+  );
+}
+
+export { ProductListingPage };
