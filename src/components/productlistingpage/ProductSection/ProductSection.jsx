a555f08ef7dc27972fd3b2953ef4be96ba623949
prajwal zingare <prajwalzingre@gmail.com>
2023-05-17 08:59:25 +0530

    feat:wraped all card in on div

--- src/components/productlistingpage/ProductSection/ProductSection.jsx
+++ src/components/productlistingpage/ProductSection/ProductSection.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import Filters from "../filters/Filters";
-// import { useProduct } from "context";
-// import ProductCard from "../productcard/ProductCard";
+import { useProduct } from "context";
+import ProductCard from "../productcard/ProductCard";
 import "./productsection.css";
 function ProductSection() {
-  // const { productData } = useProduct();
+  const { productData } = useProduct();
 
   return (
     <div className="filters-and-products">
-      <Filters />
-
-      {/* {productData.map((product) => (
-        <ProductCard product={product} key={product._id} />
-      ))} */}
+      <div>
+        <Filters />
+      </div>
+      <div className="product-cards">
+        {productData.map((product) => (
+          <ProductCard product={product} key={product._id} />
+        ))}
+      </div>
     </div>
   );
 }
