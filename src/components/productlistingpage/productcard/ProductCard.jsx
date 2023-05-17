a4906e15fcdc7b5647751e50f2b6588f7577ec4f
prajwal zingare <prajwalzingre@gmail.com>
2023-05-17 12:55:40 +0530

    feat : product card added

--- src/components/productlistingpage/productcard/ProductCard.jsx
+++ src/components/productlistingpage/productcard/ProductCard.jsx
@@ -1,11 +1,42 @@
+import { StarIcon } from "assets";
 import React from "react";
-
+import "./productcard.css";
 function ProductCard({ product }) {
-  const { title } = product;
+  const {
+    _id,
+    title,
+    description,
+    imgUrl,
+    price,
+    originalPrice,
+    rating,
+    totalReviews,
+    categoryName,
+    isOutOfStock,
+  } = product;
   return (
-    <div className="">
+    <div>
       <div className="product-card">
-        <p>{title}</p>
+        <div className="product-card-img-container">
+          <img src={imgUrl} alt="" />
+        </div>
+
+        <div className="product-card-details">
+          <p className="product-card-title">{title}</p>
+          <div className="product-card-prices">
+            <p className="price">₹{price}</p>
+            {originalPrice && (
+              <p className="original-price">₹ {originalPrice}</p>
+            )}
+          </div>
+
+          <p className="product-card-rating">
+            {rating} <StarIcon className="fp-icon" /> | {totalReviews} reviews
+          </p>
+        </div>
+        <div className="product-card-buttons">
+          <button className="card-btn">Add to Cart</button>
+        </div>
       </div>
     </div>
   );
