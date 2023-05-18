fdb6ccd97f6d11dbc697295342c9cb39decbc8d1
prajwal zingare <prajwalzingre@gmail.com>
2023-05-18 19:36:49 +0530

    add:reducer for search case

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -2,9 +2,8 @@ import {
   SORT_PRODUCTS,
   CLEAR_FILTERS,
   PRICE_RANGE,
-  STAR4_ABOVE,
-  STAR3_ABOVE,
-  STAR2_ABOVE,
+  SORT_BY_RATING,
+  SEARCH_ITEM,
 } from "constants";
 
 const reducer = (state, action) => {
@@ -13,18 +12,18 @@ const reducer = (state, action) => {
       return { ...state, sortBy: action.payload };
     case PRICE_RANGE:
       return { ...state, priceRange: action.payload };
-    case STAR4_ABOVE:
+    case SORT_BY_RATING:
       return { ...state, starRating: action.payload };
-    case STAR3_ABOVE:
-      return { ...state, starRating: action.payload };
-    case STAR2_ABOVE:
-      return { ...state, starRating: action.payload };
+    case SEARCH_ITEM:
+      return { ...state, searchText: action.payload };
+
     case CLEAR_FILTERS:
       return {
         ...state,
         sortBy: "",
         priceRange: { min: 0, max: 1110 },
         starRating: "",
+        searchText: "",
       };
     default:
       return state;
