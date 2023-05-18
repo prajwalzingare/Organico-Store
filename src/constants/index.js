eab228caa541968143228621bf2bc32b7dec020d
prajwal zingare <prajwalzingre@gmail.com>
2023-05-18 17:26:11 +0530

    feat : constatnts for price range and star rating

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,8 @@
-export { SORT_PRODUCTS, CLEAR_FILTERS } from "./constant";
+export {
+  SORT_PRODUCTS,
+  CLEAR_FILTERS,
+  PRICE_RANGE,
+  STAR4_ABOVE,
+  STAR3_ABOVE,
+  STAR2_ABOVE,
+} from "./constant";
