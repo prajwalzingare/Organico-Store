74879707bd3a4df013c85a13a43f0d17451cdc7c
prajwal zingare <prajwalzingre@gmail.com>
2023-05-19 20:46:33 +0530

    feat : route for individual page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import { NavBar, LandingPage, Footer, ProductListingPage } from "components";
+import {
+  NavBar,
+  LandingPage,
+  Footer,
+  ProductListingPage,
+  IndividualProduct,
+} from "components";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App body-container ">
       <NavBar />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/products" element={<ProductListingPage />} />
+        <Route path="/product/:productId" element={<IndividualProduct />} />
       </Routes>
       <Footer />
     </div>
