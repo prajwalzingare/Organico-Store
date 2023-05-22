b58af77fcacf9f715911868512a8390b3b46081a
prajwal zingare <prajwalzingre@gmail.com>
2023-05-22 21:00:29 +0530

    export:exported components

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,5 @@ export { Footer } from "./shared/Footer";
 export { ProductListingPage } from "./productlistingpage/ProductListingPage";
 export { IndividualProduct } from "./individualproduct/IndividualProduct";
 export { WishList } from "./wishlist/WishList";
+export { LoginForm } from "../components/authentication/login/LoginForm";
+export { SignupForm } from "../components/authentication/signup/SignupForm";
