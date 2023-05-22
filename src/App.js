e27002c70c211488b78d83ac033c4529a97f2724
prajwal zingare <prajwalzingre@gmail.com>
2023-05-22 20:59:00 +0530

    add:login and sighnup component added

--- src/App.js
+++ src/App.js
@@ -7,17 +7,25 @@ import {
   ProductListingPage,
   IndividualProduct,
   WishList,
+  LoginForm,
+  SignupForm,
 } from "components";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
 function App() {
   return (
     <div className="App body-container ">
       <NavBar />
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/signup" element={<SignupForm />} />
+
         <Route path="/products" element={<ProductListingPage />} />
         <Route path="/product/:productId" element={<IndividualProduct />} />
+
+        {/* this should be in private route  */}
         <Route path="/wishlist" element={<WishList />} />
       </Routes>
       <ToastContainer position="bottom-right" autoClose={700} draggable />
