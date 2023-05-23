ef4a7c7d731c6c9b0bc2ee5786242ae1232a5418
prajwal zingare <prajwalzingre@gmail.com>
2023-05-23 21:16:29 +0530

    refactor:userprofile

--- src/components/profile/UserProfile.jsx
+++ src/components/profile/UserProfile.jsx
@@ -1,19 +1,21 @@
 import { useAuth } from "context";
 import React from "react";
 import { Link } from "react-router-dom";
-
+import "./userprofile.css";
 function UserProfile() {
   const { logoutHandler, currentUser } = useAuth();
   return (
     <div className="flex-grow">
-      <h1>Profile</h1>
-      <h2>name</h2>
+      <div className="profile-container">
+        <h3>Full name:{currentUser?.firstName}</h3>
+        <h3>Email:{currentUser?.email}</h3>
 
-      {currentUser ? (
-        <button onClick={logoutHandler}>Logout</button>
-      ) : (
-        <Link to="/login">Login</Link>
-      )}
+        {currentUser ? (
+          <button onClick={logoutHandler}>Logout</button>
+        ) : (
+          <Link to="/login">Login</Link>
+        )}
+      </div>
     </div>
   );
 }
