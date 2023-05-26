9b420a72da352621da96e6005e2a23e794dcc427
prajwal zingare <prajwalzingre@gmail.com>
2023-05-26 14:03:18 +0530

    feat : hook for wishlist mangement

--- src/hooks/useCartAndWishlist.js
+++ src/hooks/useCartAndWishlist.js
@@ -1,16 +1,55 @@
-import { useState } from "react";
+import { UPDATE_WISHLIST } from "constants";
+import { useAuth, useData } from "context";
+
+import { useNavigate } from "react-router-dom";
+import { addToWishlist, removeFromWishlist } from "services";
 
 function useCartAndWishlist() {
-  const [isFavorite, setIsFavorite] = useState(false);
+  const { token } = useAuth();
+  const { state, dispatch } = useData();
+  const navigate = useNavigate();
 
-  const handleButtonClick = (e) => {
+  //handle wishlist Item removel from wishlist
+  const handleWishlist = async (e, product) => {
+    e.preventDefault();
+    try {
+      const res = await removeFromWishlist(token, product._id);
+      dispatch({
+        type: UPDATE_WISHLIST,
+        payload: { wishlist: res.wishlist },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  //function for checking Item isInWishlist
+  const isInWishlist = (product) =>
+    state.wishlist.find(
+      (wishlistProduct) => wishlistProduct._id === product._id
+    );
+  //function for adding and removing item from wishlist
+  const handleWishlistToggle = async (e, product) => {
     e.stopPropagation();
-    setIsFavorite(!isFavorite);
+    try {
+      if (!token) navigate("/login");
+      else {
+        const res = isInWishlist(product)
+          ? await removeFromWishlist(token, product._id)
+          : await addToWishlist(token, product);
+        dispatch({
+          type: UPDATE_WISHLIST,
+          payload: { wishlist: res.wishlist },
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return {
-    handleButtonClick,
-    isFavorite,
+    handleWishlistToggle,
+    isInWishlist,
+    handleWishlist,
   };
 }
 
