697c0233ad5882bd681cbb3faebef84b402caed5
prajwal zingare <prajwalzingre@gmail.com>
2023-06-04 11:05:58 +0530

    feat: Add adresses

---
+++ src/components/checkout/addresses.js
@@ -0,0 +1,24 @@
+const addresses = [
+  {
+    id: 1,
+    name: "Rajesh Sharma",
+    address:
+      "Flat 101, D-Block, Sunshine Apartments, Bangalore, Karnataka, 560001",
+    phoneNumber: "+91 9876543210",
+  },
+  {
+    id: 2,
+    name: "Priya Patel",
+    address:
+      "Plot 205, Sector 12, Pristine Heights, Mumbai, Maharashtra, 400001",
+    phoneNumber: "+91 9998887776",
+  },
+  {
+    id: 3,
+    name: "Amit Singh",
+    address: "House No. 304, Gali No. 6, Model Town, New Delhi, Delhi, 110001",
+    phoneNumber: "+91 8887776665",
+  },
+];
+
+export { addresses };
